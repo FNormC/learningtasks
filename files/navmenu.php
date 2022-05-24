c5412a169296cbb77359c19956bdff2ad091e4fa
FNormC <99378139+FNormC@users.noreply.github.com>
2022-05-24 12:20:51 +0900

    Update navmenu.php

--- files/navmenu.php
+++ files/navmenu.php
@@ -1,5 +1,5 @@
 <ul id="menu">
-            <li><a href="index.php?load=projects">projects</a></li>
+            <li><a href="index.php">projects</a></li>
             <li><a href="index.php?load=profile">profile</a></li>
             <li><a href="index.php?load=home">home</a></li>
         </ul> 
